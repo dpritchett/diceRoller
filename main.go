e57a7937017d9337b2af855e6d6ddc34c6cf6825
Daniel J. Pritchett <dpritchett@gmail.com>
2016-10-03 13:56:15 -0500

    Now rolling dice

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "log"
+  "math/rand"
+  "net/http"
+  "os"
+  "time"
+)
 
 func main() {
-  fmt.Println("hi")
+  rand.Seed(time.Now().UTC().UnixNano())
+
+  if(len(os.Args) < 2) {
+    log.Fatal("Usage: 'diceRoller 3000' to listen on port 3000")
+  }
+  
+  port := os.Args[1]
+
+  http.HandleFunc("/", diceHandler)
+  log.Println("Starting up on port " + port + "...")
+  log.Fatal(http.ListenAndServe(":" + port, nil))
 }
+
+func diceHandler(w http.ResponseWriter, req *http.Request) {
+  fmt.Fprintf(w, "%d", rollDice())
+}
+
+func rollDice() int {
+  return rand.Intn(6) + 1
+}
+
